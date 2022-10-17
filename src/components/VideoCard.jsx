c8de1080c28e24bdeb454c7722e1531452fa63cd
trupti0406 <truptivijayyadav27@gmail.com>
2022-10-17 22:37:28 +0530

    styling fixed

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -21,14 +21,14 @@ const VideoCard = ({
     sx={{
       width: { xs: "100%", sm: "358px", md: "320px" },
       boxShadow: "none",
-      borderRadius: 0
+      borderRadius: 0,
     }}
   >
     <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY`}>
       <CardMedia
         image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
         alt={snippet?.title}
-        sx={{ width: { xs: "100%", sm: "358px" }, height: 180 }}
+        sx={{ width: { xs: "100%", sm: "358px", md: "320px" }, height: 180 }}
       />
     </Link>
     <CardContent sx={{ backgroundColor: "#1E1E1E", height: "106px" }}>
