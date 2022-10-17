303912e0e316f6b9c0807c3d7366374f48147091
trupti0406 <truptivijayyadav27@gmail.com>
2022-10-17 22:30:25 +0530

    videodetails done

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { Stack, Box } from "@mui/material";
 import VideoCard from "./VideoCard";
 import ChannelCard from "./ChannelCard";
+import Loader from "./Loader";
 
 const Videos = ({ videos, direction }) => {
-//   if (!videos?.length) return <Loader />;
+  if (!videos?.length) return <Loader />;
 
   return (
     <Stack
